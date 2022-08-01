85515a9f8f81e5a085eec50a0dd287b575b6296a
Alejandro Giandinoto <alejandro.giandinoto@gmail.com>
2022-08-01 16:46:06 -0300

    Boilerplate

---
+++ src/edit.js
@@ -0,0 +1,77 @@
+import { __ } from '@wordpress/i18n';
+import { useBlockProps, RichText, BlockControls } from '@wordpress/block-editor';
+import { ToolbarGroup, ToolbarButton, DropdownMenu } from '@wordpress/components';
+import './editor.scss';
+
+export default function Edit( { attributes, setAttributes } ) {
+
+	const { text } = attributes;
+
+	return (
+		<>
+			<BlockControls group="inline">
+				<p>Inline</p>
+			</BlockControls>
+
+			<BlockControls group="block">
+				<p>Block controls</p>
+			</BlockControls>
+			
+			<BlockControls 
+				controls={[{
+				title: "Button 1",
+				icon: "admin-generic",
+				isActive: true,
+				onClick: () => console.log("Botton 1 click")
+			}]} >
+				<ToolbarGroup>
+					<p><b>Title</b></p>	
+					<ToolbarButton 
+						title = "Align left"
+						icon="editor-alignleft"
+						onclick =  { () => console.log("align left") } 
+						
+					/>
+
+					<ToolbarButton 
+						title = "Align center"
+						icon="editor-aligncenter"
+						onclick =  { () => console.log("align center") } 
+					/>
+
+					<ToolbarButton 
+						title = "Align Right"
+						icon="editor-alignright"
+						onclick =  { () => console.log("align Right") } 
+					/>
+
+					<DropdownMenu
+						icon="arrow-down-alt2"
+						label = { __( "More alignments", "text-box")}
+						controls = { [
+							{
+								title: __( "wide", "text-box"),
+								icon: "align-wide",
+								onClick: () => console.log("Wide")
+							},
+							{
+								title: __( "Full", "text-box"),
+								icon: "align-full-wide",
+								onClick: () => console.log("Full")
+							}		
+						] }
+					/>
+				</ToolbarGroup>
+			
+			</BlockControls>
+
+			<RichText  { ...useBlockProps() } 
+				onChange = { ( text ) => setAttributes ( { text } ) }
+				value = { text }
+				placeholder={ __("Placeholder", "text-block")}
+				tagName="h4"
+				allowedFormats={[ ] }
+			/>
+		</>
+	);
+}
